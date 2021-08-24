324dba137652694a3d4cf8b395b338a65e71f310
nicolasgallandmonde <galland@ext.lemonde.fr>
2021-08-24 20:01:41 +0000

    v2 système d'alerte maintenant basé sur des notebooks schedulés

--- commons_alerts.py
+++ commons_alerts.py
@@ -1,19 +1,20 @@
 import jmespath, requests, base64, json
+import cachetools.func
 
 def get_credentials():
-    return  json.loads(open('/.credentials.json','r').read())
+    return  json.loads(open('/workspace/.credentials.json','r').read())
 
 
 #------------------------- Slack
 import os
-from slack import WebClient
-from slack.errors import SlackApiError
+from slack_sdk import WebClient
+
 
 _slack = {}
-def init_slack(log):
+def init_slack():
     _slack['client'] = WebClient(token=(get_credentials())['slack']['xoxb'] )
 
-def send_slack(log, text, channel):
+def send_slack(text, channel):
     """Send slack message in a public channel"""
     return _slack['client'].chat_postMessage(
         channel=channel,
@@ -32,7 +33,7 @@ client = None
 sheets = {}
 spreadsheets = {}
 
-def init_google_sheets(log, credentials_file):
+def init_google_sheets(credentials_file):
     """Read credentials in the GSheetsCredentials.json file, and init the connection """
     global client,sheets,spreadsheets
     sheets = {}
@@ -64,7 +65,7 @@ def _exp2D(tab, sheet, cell):
         cell.value = tab [math.floor(i/nbc)] [i%nbc]
     sheet.update_cells(_range)
 
-def send_google_sheet(log, spreadsheet, sheet, cell, tab):
+def send_google_sheet(spreadsheet, sheet, cell, tab):
     """send a 2D array to a specific cell in a specific sheet (by sheet name) in a specific spreadsheet (by spreadsheet name)"""
     _exp2D(tab,_getSheet(_getSpreadsheet(spreadsheet),sheet),cell)
 
@@ -107,10 +108,10 @@ def _case_amplitude_event_segmentation(json):
             row.append(y[i])
         out.append(row)
     return out
-    
+
 def _case_amplitude_funnel(json):
     """Interpret ampllitude json response in the case of a funnel chart """
-    dates = jmespath.search('data[0].dayFunnels.xValues', json) 
+    dates = jmespath.search('data[0].dayFunnels.xValues', json)
     values = jmespath.search('data[0].dayFunnels.series', json)
     rates = list(map(lambda a: 0 if a[0] == 0 else 100.*a[1]/a[0] , values))
     return list(map(lambda date, rate: [date,rate], dates, rates))
@@ -121,8 +122,13 @@ def _case_amplitude_formula(json):
     values = jmespath.search('data.series[0][*].value', json)
     return list(map(lambda date, value : [date,value], dates, values))
 
-def amplitude_to_array(log, amplitude_chart_id):
-    """Makes the request to amplitude, interpret response, and return a data array. 
+
+@cachetools.func.ttl_cache(maxsize=528, ttl=60)
+def amplitude(amplitude_chart_id):
+    return amplitude_to_array(amplitude_chart_id)
+    
+def amplitude_to_array(amplitude_chart_id):
+    """Makes the request to amplitude, interpret response, and return a data array.
     Arg amplitude_chart_id : the chart id you can find in the url of a SAVED chart"""
     credentials_amplitude = (get_credentials())['amplitude']
     userpass = credentials_amplitude["API_KEY"]+':'+credentials_amplitude["API_SECRET"]
@@ -136,7 +142,7 @@ def amplitude_to_array(log, amplitude_chart_id):
     if r.status_code != 200:
         raise Exception (f"Erreur lors de la requête à amplitude. Erreur {r.status_code}")
     json = r.json()
-    #cas funnel 
+    #cas funnel
     if jmespath.search('data[0].dayFunnels.xValues', json):
         return _case_amplitude_funnel(json)
     #case user segmentation
@@ -149,7 +155,13 @@ def amplitude_to_array(log, amplitude_chart_id):
         raise Exception("Type de chart amplitude non géré (ni tunnel simple, ni event segmentation simple)")
 
 #---------------------------- AT
-def AT_to_array(log, url):
+
+@cachetools.func.ttl_cache(maxsize=528, ttl=60) 
+def AT(url):
+    return AT_to_array(url)
+
+
+def AT_to_array(url):
     """Makes the request to amplitude, interpret response, and return a data array """
     url = url.replace('/html/', '/json')
     credentials = (get_credentials())['AT']
@@ -162,7 +174,6 @@ def AT_to_array(log, url):
     try:
         json = r.json()
     except Exception as e:
-        log.info(r.text)
         raise e
     data = json['DataFeed'][0]
     col_names = jmespath.search('Columns[*].Label', data)
@@ -175,3 +186,79 @@ def AT_to_array(log, url):
             row.append(values[j][i])
         out.append(row)
     return out
+
+
+#------------------------------ Alertes
+def get_alerte_amplitude(logger):
+    def alerte_amplitude(chart_id=None, channel=None, floor=None, ceil=None):
+        def inner(func):
+            def alert(comment=None, value=None):
+                s_ceil = '' if ceil == None else ' seuil haut: '+str(round(ceil,2))
+                s_floor = '' if floor == None else ' seuil bas: '+str(round(floor,2))
+                s_value = '' if value == None else ' valeur: '+str(round(value,2))
+                message = f"Alerte ! {func.__doc__}" +\
+                    f"{':' if ceil!=None or floor!=None or value!=None else ''} {s_value} {s_floor} {s_ceil} "+\
+                    f"{comment if comment!=None else ''}"+\
+                    f"\nDonnées utilisées : https://analytics.amplitude.com/lemonde/chart/{chart_id}"
+                if channel != None:
+                    try:
+                        send_slack(message, channel)
+                    except:
+                        send_slack("Impossible d'envoyer un message au channel: "+channel, "alertbot-erreurs-techniques")
+                logger.error(message)
+            if func.__doc__ == None:
+                raise Exception("SVP ajoutez une doc à la fonction")
+            logger.info('------------------- ' + func.__doc__)
+            if channel == None:
+                logger.error("Warning: Pas de channel slack défini pour l'alerte "+ func.__doc__)
+            data = amplitude(chart_id)
+
+            dates = list(map(lambda r:r[0], data))
+            values = list(map(lambda r:r[1], data))
+            value = func(dates, values, data, alert)
+            if value != None and floor != None and value <= floor:
+                alert(value=value)
+            if value != None and ceil != None and value >= ceil :
+                alert(value=value)
+            logger.info(f"Amplitude : chart_id: {chart_id} - value: {value} - ceil: {ceil} - floor:{floor} - channel: {channel} ")
+            #print values
+            list(map(lambda d: logger.info(d), data))
+        return inner
+    return alerte_amplitude
+
+
+
+def get_alerte_AT(logger):
+    def alerte_AT(chart_id=None, channel=None, floor=None, ceil=None):
+        def inner(func):
+            def alert(comment=None, value=None):
+                s_ceil = '' if ceil == None else ' seuil haut: '+str(round(ceil,2))
+                s_floor = '' if floor == None else ' seuil bas: '+str(round(floor,2))
+                s_value = '' if value == None else ' valeur: '+str(round(value,2))
+                message = f"Alerte ! {func.__doc__}" +\
+                    f"{':' if ceil!=None or floor!=None or value!=None else ''} {s_value} {s_floor} {s_ceil} "+\
+                    f"{comment if comment!=None else ''}"+\
+                    f"\nDonnées utilisées : {chart_id}"
+                if channel != None:
+                    try:
+                        send_slack(message, channel)
+                    except:
+                        send_slack("Impossible d'envoyer un message au channel: "+channel, "alertbot-erreurs-techniques")
+                logger.error(message)
+            if func.__doc__ == None:
+                raise Exception("SVP ajoutez une doc à la fonction")
+            logger.info('------------------- ' + func.__doc__)
+            if channel == None:
+                logger.error("Warning: Pas de channel slack défini pour l'alerte "+ func.__doc__)
+            data = AT_to_array(chart_id)
+            value = func(data, alert)
+            if value != None and floor != None and value <= floor:
+                alert(value=value)
+            if value != None and ceil != None and value >= ceil :
+                alert(value=value)
+            logger.info(f"AT : chart_id: {chart_id} - value: {value} - ceil: {ceil} - floor:{floor} - channel: {channel} ")
+            #print values
+            list(map(lambda d: logger.info(d), data))
+        return inner
+    return alerte_AT
+
--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-dagster-daemon run &
-dagit -h "0.0.0.0" -p "3000" -w "/opt/dagster/app/workspace.yaml"
